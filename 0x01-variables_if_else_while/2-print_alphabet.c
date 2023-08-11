2ba55df9d41b308a55ae0cf99fbc55fa7f657601
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-11 23:39:19 +0300

    playing wiht alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*
+main functin-starting point 
+returns 0
+*/
+int main()
+{
+char letter='a';
+while(letter<='z')
+{
+    putchar(letter);
+    letter++;  
+}
+printf("\n");
+}
