284661a931b12ded9589ad147e3299302640f2f2
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-11 22:30:12 +0300

    chek the sign of random no

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 #include <time.h>
 /* more headers go here */
-
 /* betty style doc for function main goes here */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int n;
-
     srand(time(0));
     n = rand() - RAND_MAX / 2;
     if (n > 0)
