d3fc3f94ee6ad23035af96142bd79d573cba5458
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 23:32:20 +0300

    final work

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/*
+ * main - starting point
+ *
+ * Description : print all combination of two two-digit numbers
+ *
+ * return 0
+ * */
+int main()
+{	int i;
+	int j;
+	int k;
+	int z;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for ( k = 0; k <= 9; k++)
+			{
+				for ( z = 0; z <= 9; z++)
+				{	
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(z);
+			  	if( i != 9 || j != 8 || k != 9 || z != 9 )
+				{
+					putchar(',');
+			 	}	
+			 	}	
+			}	
+				
+		}	
+	}
+return (0);	
+}	
+
