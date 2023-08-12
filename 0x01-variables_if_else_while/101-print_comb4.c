5fd670b3537a91c0fb36335a64430839e6f6f6b5
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 22:29:57 +0300

    combination of three digit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,21 +16,22 @@ int k;
 
 for (i = 0; i <= 7; i++)
 {
-for (j = i + 1; j <= 8; j++)
-{
-for (k = j + 1; k <= 9; k++)
-{
-putchar('0' + i);
-putchar('0' + j);
-putchar('0' + k);
-if (i != 7 || j != 8 || k != 9)
-{
-putchar(',');
-putchar(' ');
+    for (j = i + 1; j <= 8; j++)
+    {
+        for (k = j + 1; k <= 9; k++)
+        {
+            putchar('0' + i);
+            putchar('0' + j);
+            putchar('0' + k);
+
+        if (i != 7 || j != 8 || k != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        }
+    }
 }
-}
-}
-}
-putchar('\n');
-return (0);
+    putchar('\n');
+    return (0);
 }
