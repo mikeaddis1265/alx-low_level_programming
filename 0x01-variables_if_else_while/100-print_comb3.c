1cb0b07c1df318b9a4066512c3d7f2e0e99cae16
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 21:44:10 +0300

    possible combination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,23 +5,23 @@
  *
  *Return: 0
  *
- *Descripitions: Prints all possible combinations of single-digit numbers
+ *Descripitions: Prints all possible combinations of two-digit numbers
  */
 
 int main(void)
 {
-   int i ;
-   int j = 0;
+int i ;
+int j = 0;
 
-   for (i = 0; i <= 9; i++) 
-   {
-    for (j = 0; j <= 9; j++)
-    {
-        putchar('0' + i);
-        putchar('0' + j);
-        putchar(',');
-        putchar(' ');
-    }
-   }
+for (i = 0; i <= 8; i++) 
+{
+for (j = 0; j <= 9; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar(',');
+putchar(' ');
+}
+}
 return (0);   
 } 
