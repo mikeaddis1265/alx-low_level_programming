171cd49a285f60b847d3d6e7c19a36f7aa235469
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 18:01:51 +0300

    chek the sign of random no

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,40 +1,39 @@
 #include <stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
 #include <time.h>
+
 /*
- *main - Starting point
- *Descriptions : getting the last digit of the random number
- *Return: 0
-*/
+ * main - Starting point
+ * Description: getting the last digit of the random number
+ * Return: 0
+ */
 int main(void)
 {
+    int n;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if(n >= 0)
-{
-    if(n % 10 > 5)
+    /* your code goes here */
+    if (n >= 0)
     {
-        printf("Last digit of %d is %d and is greater than 5\n",n,n % 10);
+        if (n % 10 > 5)
+        {
+            printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+        }
+        else if (n % 10 < 6 && (n % 10) != 0)
+        {
+            printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+        }
+        else
+        {
+            printf("Last digit of %d is %d and is 0\n", n, n % 10);
+        }
     }
-    else if(n % 10 < 6 && (n % 10) != 0)
-    {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n",n,n % 10);
-    } 
     else
-	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}
-}
-else
-{   
-        int x = 0-(n % 10);
-        printf("Last digit of %d is -%d and is less than 6 and not 0\n",n,x);
-   
-}
+    {
+        int x = 0 - (n % 10);
+        printf("Last digit of %d is -%d and is less than 6 and not 0\n", n, x);
+    }
 
-return (0);
-
+    return (0);
 }
