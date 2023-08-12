e4c02f119ed2e1bf2016caf963dfe98140852c76
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-13 00:27:49 +0300

    fina project

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,11 +20,11 @@ int main()
 			{
 				for ( z = 0; z <= 9; z++)
 				{	
-					putchar(i);
-					putchar(j);
+					putchar(i + '0');
+					putchar(j + '0');
 					putchar(' ');
-					putchar(k);
-					putchar(z);
+					putchar(k + '0');
+					putchar(z + '0');
 			  	if( i != 9 || j != 8 || k != 9 || z != 9 )
 				{
 					putchar(',');
