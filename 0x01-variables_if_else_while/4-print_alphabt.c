bd18bb37fbb62de88cdbff9a2ede1528be58c7f6
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 19:05:17 +0300

    alphabet practice

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Starting point
+ *
+ * Description: 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char letter = 'a';
+while(letter <= 'z')
+{
+if(letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
+letter++;
+}
+putchar('\n');
+return(0);
+}
