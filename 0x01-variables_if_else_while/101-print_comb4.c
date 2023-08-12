6d62015415e4440888c4c9bb8da74dbfba24d8ff
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 22:27:48 +0300

    combination of three digit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
  *
  *Return: 0
  *
- *Descripitions: Prints all possible combinations of two-digit numbers
+ *Descripitions: Prints all possible combinations of three-digit numbers
  */
 
 int main(void)
@@ -23,7 +23,7 @@ for (k = j + 1; k <= 9; k++)
 putchar('0' + i);
 putchar('0' + j);
 putchar('0' + k);
-if (i != 7 || j != 8 || k != 9 )
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
