6245d4c3f516bd0a305af49b914ffe318809ba78
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 18:19:02 +0300

    chek the sign of random no

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
-
+/**
+ * main - Starting point
+ *
+ * Description: display all alphabet
+ *
+ * Return: 0
+ */
 int main()
 {
 char letter='a';
 while(letter<='z')
 {
-    putchar(letter);
-    letter++;  
+putchar(letter);
+letter++;  
 }
 printf("\n");
 return(0);
