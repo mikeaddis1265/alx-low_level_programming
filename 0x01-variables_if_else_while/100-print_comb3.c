4e42b1ad27a7e29311e3a7d1c073f9509916ed0b
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 21:53:15 +0300

    possible combination of two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,22 +10,22 @@
 
 int main(void)
 {
-int i ;
+int i;
 int j = 0;
 
-for (i = 0; i <= 8; i++) 
+for (i = 0; i <= 8; i++)
 {
-for (j = 0; j <= 9; j++)
+for (j = 1; j <= 9; j++)
 {
 putchar('0' + i);
 putchar('0' + j);
-if (i != 8 || j != 9 )
+if (i != 8 || j != 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
-putchar('\n'); 
-return (0); 
-} 
+putchar('\n');
+return (0);
+}
