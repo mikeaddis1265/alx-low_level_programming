8f2a3379dee0b937dc3d2ab91a7e696187895298
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-12 20:19:26 +0300

    letters in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Starting point
+ *
+ * Description: print reverse order
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char letter = 'z';
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+return (0);
+}
