4297ab9d38b774e6ff398d569179a52e1bd359f2
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-15 21:12:58 +0300

    noting

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * using header file to use programmers' functions 
+ * using header file to use programmers' functions
  * main -starting point function
  * Description -a program to print a word
  * exit main-after execution the main function will close
@@ -9,7 +9,7 @@
 int main(void)
 {
 	char name[] = "_putchar";
-	int i = 0;	
+	int i = 0;
 	/*use for loop to print out each character*/
 	for (; i < 8; i++)
 	{
