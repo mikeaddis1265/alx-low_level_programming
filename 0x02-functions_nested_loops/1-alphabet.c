0eb7054a5841bdfb3d1051c5fb7c05644984d0b9
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-15 21:34:49 +0300

    print letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Starting point
+ *
+ * Description: display all alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{ char letter = 'a';
+     
+         while (letter <= 'z')
+        {
+                _putchar(letter);
+                 letter++;
+        }
+   
+ return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,17 @@
 #define MAIN_H
 
 int _putchar(char c);
+void print_alphabet(void);
+
+/* function to print alphbets body */
+void print_alphabet(void)
+{
+	char letter 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		 letter ++;
+return;
+}
 
 #endif /* MAIN_H */
