62933d480fe4aa747988e4086ca44c866dd11a93
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-16 09:47:36 +0300

    this my readk

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,6 @@
 
 #include "main.h"
-/**
- * main - Starting point
- *
- * Description: display all alphabet
- *
- * Return: 0
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+
 /*
 print_alphabet - Starting point
  *
