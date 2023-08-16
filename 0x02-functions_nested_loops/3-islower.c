58656561e8d8f333caeeacfb72847d450cf1378f
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-16 17:17:39 +0300

    update fun

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /*
- * _:islower.c -is a function to check is a character is small lertter of not
+ * _islower.c -is a function to check is a character is small lertter of not
  * Returns 1 if c is lowercass
  * returns o otherwise
  */
 int _islower(int c)
+	/* the function inputs and returns an integer type*/
 {
 	if (c <= 122 && c >= 97)
 	{
@@ -15,3 +16,4 @@ int _islower(int c)
 		return (0);
 	}
 }
+
