12eec6c35d852b2b88dc1bc17545016cebf422df
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-16 09:18:34 +0300

    debugg 1-alpbet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,43 +1,32 @@
 
-// #include "main.h"
-
-// /**
-//  * main - Starting point
-//  *
-//  * Description: display all alphabet
-//  *
-//  * Return: 0
-//  */
-// int main(void)
-// {
-//     print_alphabet();    
-//     return (0);
-// }
-
-// void print_alphabet(void)
-// {
-//     char letter = 'a';
-//     while (letter <= 'z')
-//     {
-//         _putchar((letter)); // Corrected this line to pass the variable letter
-//         letter++;
-//     }
-// }
 #include "main.h"
+int _putchar(char c);
 
 /**
- *print_alphabet - prints alphabets in lowercase
+ * main - Starting point
  *
- *Return: Always 0.
+ * Description: display all alphabet
+ *
+ * Return: 0
  */
-
+int main(void)
+{
+    print_alphabet();    
+    return (0);
+}
+/*funtion to proint one character*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/*funtion to print all small letter alphbets*/  
 void print_alphabet(void)
 {
-int i;
-
-for (i = 97; i <= 122; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
+    char letter = 'a';
+    while (letter <= 'z')
+    {
+        _putchar(letter); // Corrected this line to pass the variable letter
+       letter++;
+    }
+    _putchar('\n');
 }
