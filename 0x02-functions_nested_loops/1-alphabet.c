18e07504359daf80ca12bd4fc12f94cc9d85c66f
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-16 09:02:30 +0300

    modify a file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,43 @@
+
+// #include "main.h"
+
+// /**
+//  * main - Starting point
+//  *
+//  * Description: display all alphabet
+//  *
+//  * Return: 0
+//  */
+// int main(void)
+// {
+//     print_alphabet();    
+//     return (0);
+// }
+
+// void print_alphabet(void)
+// {
+//     char letter = 'a';
+//     while (letter <= 'z')
+//     {
+//         _putchar((letter)); // Corrected this line to pass the variable letter
+//         letter++;
+//     }
+// }
 #include "main.h"
+
 /**
- * main - Starting point
+ *print_alphabet - prints alphabets in lowercase
  *
- * Description: display all alphabet
- *
- * Return: 0
+ *Return: Always 0.
  */
 
-int main(void)
-{ char letter = 'a';
-     
-         while (letter <= 'z')
-        {
-                _putchar(letter);
-                 letter++;
-        }
-   
- return (0);
+void print_alphabet(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,15 +5,4 @@
 int _putchar(char c);
 void print_alphabet(void);
 
-/* function to print alphbets body */
-void print_alphabet(void)
-{
-	char letter 'a';
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		 letter ++;
-return;
-}
-
-#endif /* MAIN_H */
+#endif/* MAIN_H */
