58ebbb0568782ed357347228a3ef706f3e1ac4d1
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-16 17:06:59 +0300

    function to chek a char if it is small letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * 3-islower.c -is a function to check is a character is small lertter of not
+ * Returns 1 if c is lowercass
+ * returns o otherwise
+ */
+int _islower(int c)
+{
+	if (c <= 122 && c >= 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
