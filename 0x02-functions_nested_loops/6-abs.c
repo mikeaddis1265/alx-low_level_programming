4b58a9a1eb9583568b10b408c57b3e4651b8bf75
mikeaddis1265 <124905330+mikeaddis1265@users.noreply.github.com>
2023-08-16 22:05:20 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * abc -get absolute value of number
+ * abc -get the absolute value of the number
  *
  * n : int type
  *
  * Return: 1 and print + if number is greater than zero
- * Return: 0 and prints 0 if n is zero
+ * Return: 0 and print 0 if n is zero
  * Return: -1 and prints - if n is less than zero
  */
-int _abs(int n )
+int _abs(int n)
 {
-	return (n >= 0) ? n : -n;
-
+	return ((n >= 0) ? n : -n;)
 }
-
