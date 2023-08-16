36039b3db9e7fd851868ceffdcbf990719ed48fc
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-16 10:58:29 +0300

    update version of print_aplphabet function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * 2-print_alphabet_x10 -prints smallleter alphabets 10 times
+ * return - always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	while (i<10)
+	{
+		print_alphabet();/* call the upper function */
+		i++;
+	}
+	
+	
+}
+ 
