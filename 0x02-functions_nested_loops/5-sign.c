6be91a4edd041fafb1b8f7976c617318cea2feee
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-16 20:23:29 +0300

    print sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _isalpha -checks for sign of an integer number
+ *
+ * input : int type 
+ *
+ * Return: 1 and print + if number is greater than zero
+ * Return: 0 and prints 0 if n is zero
+ * Return: -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,8 @@ void print_alphabet_x10(void);
 int _islower(int c);
 /*function to check if a symbol is a letter or not */
 int _isalpha(int c);
+/*function to print sign of a number */
+int print_sign(int n);
 #endif /* MAIN_H */
 
 
