0c382c9668345ca87a501a631cf2bcdab74d26f1
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-18 00:14:33 +0300

    print number fun

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers-playing with numbers
+ *
+ * Return- noting
+ *
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (; i < 10; i ++)
+	{
+		if ((i != 2) && (i != 4))
+		{
+			_putchar(i + '0');
+		}	
+	}
+	_putchar('\n');
+}
