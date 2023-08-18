d599e666bf627252f4538beede148e5d3a42b303
mikeaddis1265 <124905330+mikeaddis1265@users.noreply.github.com>
2023-08-18 14:58:02 +0300

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+ *
+ * main -entry point
+ *
+ * Return -always 0
+ *
+ */
+
+int main(void)
+{
+        unsigned int long n = 612852475143;
+         unsigned int long maxprime = -1;
+
+        /*divide the nummber by 2 until it becomes odd*/
+
+        while (n % 2 == 0)
+        {
+                maxprime = 2;
+                n = n / 2;
+        }
+
+        /*try dividing by odd numbers starting from 3*/
+
+        int i = 3;
+        for (; i <= sqrt(n); i = i + 2)
+        {
+                while(n % i == 0)
+                {
+                        maxprime = i;
+                        n = n / i;
+                }
+
+        }
+         if (n > 2)
+        {
+            maxprime = n;
+        }
+
+        printf("%lu\n", maxprime);
+return (0);
+}
