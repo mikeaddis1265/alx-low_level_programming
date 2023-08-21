b54c611aab824077fa0e7849f4f3bd3991d8f690
mikeaddis1265 <124905330+mikeaddis1265@users.noreply.github.com>
2023-08-21 18:05:09 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *reset_to_98 - is a function with pointer parameter and update the value of variable it points to
+ *reset_to_98 - is a function with a pointer parameter and updates the value of the variable it 
  *
- * @*n - is a pointer parameter ,points to integer type
+ * @*n - is a pointer parameter,points to an integer type
  *
  * Return - void
  *
