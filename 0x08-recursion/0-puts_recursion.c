2198421d2719b74ae52613148cec1d03c69533e1
mikeaddis1265 <michaeladdis78@gmail.com>
2023-08-30 16:04:34 +0300

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts_recursion : put string to console
+ *
+ * @s : pointr to the string
+ *
+ * Return : nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
+	else
+	{
+	    _putchar('\n');
+	}
+}
+
