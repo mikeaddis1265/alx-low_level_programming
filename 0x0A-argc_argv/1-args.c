2d39dd7f0b033a2f48cfd943ed68b487fd3b35c6
mikeaddis1265 <michaeladdis78@gmail.com>
2023-09-03 13:48:32 +0300

    num of argumetns

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print the number of arguements you passed into it
+ *
+ * @argc: An argument counter
+ *
+ * @argv: An argurmnt values
+ *
+ * Return: Always o (success)
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		i++;
+	}
+	printf("%d\n", i);
+	return (0);
+}
