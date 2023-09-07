2e04e38d68380a3401330e1ae4588a834fb10f60
mikeaddis1265 <michaeladdis78@gmail.com>
2023-09-07 11:42:35 +0300

    create dynamic memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocate a memory using malloc
+ *
+ * @b : size of memory to be allocated
+ *
+ * Return : pointer to the base address of the memory 
+ *
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+		
+}
