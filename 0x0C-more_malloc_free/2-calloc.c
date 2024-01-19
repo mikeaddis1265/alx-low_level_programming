7bc80bb50df74cb2c94b9787fca0184387bc8f24
mikeaddis1265 <michaeladdis78@gmail.com>
2024-01-19 21:31:54 +0300

    fix bugs

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -26,4 +26,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		p[i] = 0;
 
 	return (p);
+
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * array_range - a function that creates an array of integers.
+ * @min: min value in array list.
+ * @max: max value in array list.
+ *
+ * Return: pointer to newly created array
+ */
 int *array_range(int min, int max) {
+    int *arr;
+    int size, i;
+
     if (min > max) {
         return NULL;
     }
 
-    int size = max - min + 1;
-    int *arr = malloc(sizeof(int) * size);
+    size = max - min + 1;
+    arr = malloc(sizeof(int) * size);
 
     if (arr == NULL) {
-        printf("Error: Memory allocation failed\n");
         return NULL;
     }
 
-    for (int i = 0; i < size; i++) {
-        arr[i] = min + i;
+    for (i = 0; i < size; i++) {
+        arr[i] = min++;
     }
 
     return arr;
