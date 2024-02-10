341e118c97205aae5fa575293ee0f11bc82d62b1
mikeaddis1265 <michaeladdis78@gmail.com>
2024-02-10 18:26:05 +0300

    done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+/*
+ * binary_to_uint- converts a binary number to unsigned int.
+ *
+ * @b: pointer to string of 0 and 1 chars.
+ *
+ * Return: the converted number.
+ */
+
+unsigned int power(unsigned int base, unsigned int expo)
+{
+    unsigned int result = 1;
+    while (expo > 0)
+    {
+        result *= base;
+        expo--;
+    }
+    return result;
+}
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int numberOfbits = 0, number = 0, i;
+    const char *ptr;
+
+    if (b == NULL)
+        return 0;
+
+    ptr = b;
+    while (*ptr != '\0')
+    {
+        if (*ptr != '0' && *ptr != '1')
+            return 0;
+        numberOfbits++;
+        ptr++;
+    }
+
+    ptr = b; 
+	numberOfbits--;
+   for (i = numberOfbits; i > 0; i--)
+    {
+        if (*ptr == '1')
+            number += power(2, i);
+
+        ptr++;
+    }
+
+    return number;
+}
---
+++ test/bitOperator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 4;
+    int b = 9;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("a&b = %d\n", a & b);
+    printf("a | b = %d\n", a | b);
+    printf("a ^ b = %d\n", a ^ b);
+    printf("~a = %d\n", a = ~a);
+    printf("b<<1 = %d\n", b << 1);
+    printf("b>>1 = %d\n", b >> 1);
+
+
+
+}
