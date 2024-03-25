1870c1e88d26f92756ded2931daf47e5e50feff0
mikeaddis1265 <michaeladdis78@gmail.com>
2024-03-25 03:17:14 +0300

    update

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,23 +10,24 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i;
+	dlistint_t *temp;
+	dlistint_t *nth;
+	unsigned int count;
 
-	if (head == NULL)
-		return (NULL);
-
-	while (head->prev != NULL)
-		head = head->prev;
-
-	i = 0;
-
-	while (head != NULL)
+	temp = head;
+	count = 0;
+	for (count = 0; count <= index; count++)
 	{
-		if (i == index)
-			break;
-		head = head->next;
-		i++;
+		if (temp != NULL)
+		{
+			temp = temp->next;
+		}
+		else
+		{
+			return (NULL);
+		}
 	}
-
-	return (head);
+	nth = temp->prev;
+	return (nth);
 }
+
