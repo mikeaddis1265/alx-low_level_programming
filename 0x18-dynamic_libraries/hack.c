29ee49e2024f7989310e591d4f95c802a725b506
mikeaddis1265 <michaeladdis78@gmail.com>
2024-07-10 22:56:11 +0300

    done

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -4,7 +4,7 @@
 
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 + 9\n", 17);
-	write(1, "Congratulations, you win the Jackpot1!\n", 38);
-	exit(EXIT_SUCCESS);
+    write(1, "9 8 10 24 75 + 9\n", 17);
+    write(1, "Congratulations, you win the Jackpot1!\n", 39); // Added \n at the end
+    exit(EXIT_SUCCESS);
 }
